121b05a3b1921bd068f3d74694bededdbee29e3d
chaeyeong1219 <110645845+chaeyeong1219@users.noreply.github.com>
2025-02-24 15:57:42 +0900

    [level 1] Title: 시저 암호, Time: 2.63 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,20 +1,15 @@
 class Solution {
     public String solution(String s, int n) {
-        n = n % 26;
-        
-        StringBuilder result = new StringBuilder();
+        n %= 26;     
+        StringBuilder result = new StringBuilder(s.length());
         
         for (char ch : s.toCharArray()) {
             if (Character.isUpperCase(ch)) {
-                result.append((char)('A' + (ch - 'A' + n) % 26));
-            }
-            
-            if (Character.isLowerCase(ch)) {
-                result.append((char)('a' + (ch - 'a' + n) % 26));
-            }
-            
-            if (ch == ' ') {
-                result.append(' ');
+                result.append((char) ((ch - 'A' + n) % 26 + 'A'));
+            } else if (Character.isLowerCase(ch)) {
+                result.append((char) ((ch - 'a' + n) % 26 + 'a'));
+            } else {
+                result.append(ch);
             }
         }
         
