7ef5eca11e81e3daa95e14a932ddfe2b83c429c6
chaeyeong1219 <110645845+chaeyeong1219@users.noreply.github.com>
2025-02-24 11:48:18 +0900

    [level 1] Title: 예산, Time: 0.53 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int total = 0;
+        
+        Arrays.sort(d);
+        
+        for (int i = 0; i < d.length; i++) {
+            total += d[i];
+            if (total > budget) {
+                answer = i;
+                break;
+            }
+        }
+        
+        if (total <= budget) {
+            answer = d.length;
+        }
+               
+       return answer;
+    }
+}
