1aeec93dc07716d321da306ff6c3eba6c5aa4208
chaeyeong1219 <110645845+chaeyeong1219@users.noreply.github.com>
2025-02-25 17:55:20 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 12.97 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < food.length; i++) {
+            if (food[i] % 2 != 0) {
+                food[i] -= 1;
+            }
+
+            for (int j = 0; j < food[i] / 2; j++) {
+                sb.append(i);
+            }
+        }  
+
+        String answer = sb.toString() + "0" + sb.reverse().toString();
+        
+        return answer;
+    }
+}
