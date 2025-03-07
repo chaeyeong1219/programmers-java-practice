878a1246964fef6e5ec69c8bf97af512d0f0ba65
윤채영(Yoon Chaeyeong) <110645845+chaeyeong1219@users.noreply.github.com>
2025-03-07 11:48:36 +0900

    [level 1] Title: 모의고사, Time: 6.76 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int[] answers) {            
+        int[] spj1 = {1, 2, 3, 4, 5};
+        int[] spj2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] spj3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        List<Integer> result = new ArrayList<>();
+
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+        Queue<Integer> q3 = new LinkedList<>();
+
+        for (int i : spj1) {
+            q1.add(i);
+        }
+        for (int i : spj2) {
+            q2.add(i);
+        }
+        for (int i : spj3) {
+            q3.add(i);
+        }
+
+        int cnt1 = 0, cnt2 = 0, cnt3 = 0;
+        for (int i = 0; i < answers.length; i++) {
+            if (q1.peek() == answers[i]) cnt1++;
+            q1.offer(q1.poll());
+
+            if (q2.peek() == answers[i]) cnt2++;
+            q2.offer(q2.poll());
+
+            if (q3.peek() == answers[i]) cnt3++;
+            q3.offer(q3.poll());
+
+        }
+
+        int best = Math.max(cnt1, Math.max(cnt2, Math.max(cnt3, cnt3)));
+        if (best == cnt1) {
+            result.add(1);
+        }
+        if (best == cnt2) {
+            result.add(2);
+        }
+        if (best == cnt3) {
+            result.add(3);
+        }
+        
+        return result;
+        
+    }
+}
